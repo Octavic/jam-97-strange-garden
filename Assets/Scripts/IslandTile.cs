b98efef08fc3c5acf51959bcd6ab5d5f2cf96750
Octavic <yifeixu@live.com>
2019-05-20 00:01:49 -0700

    Add basic hover mechanics

--- Assets/Scripts/IslandTile.cs
+++ Assets/Scripts/IslandTile.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IslandTile : MonoBehaviour
+public class IslandTile : BaseInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public override void OnClick()
+	{
+		Debug.Log("CLicked on island");
+	}
 }
